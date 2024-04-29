fe33872350febfc49188531ebba5dbf37668aa70
Brenda <brendarodrigues2022@gmail.com>
2024-04-29 16:21:36 -0300

    settings

--- arquivo.js
+++ arquivo.js
@@ -2,53 +2,55 @@
 
 // funçaõ para que quando clicar no número ele aparecer na tela e acresentar o proximo que eu digitar
 
-let firstValue = '';
-let secondValue = '';
 let lista = ['', ''];
 let simbol = '';
 let restart = false;
 
 
 
-
-
 function insert(num) {
+
+  if (num === ',') {
+    // Se o último caractere do número atual for uma vírgula, não faça nada
+    if (lista[0].endsWith(',') || lista[1].endsWith(',')) {
+      return;
+    }
+  }
   if (lista.length == 0 || simbol == '') {
     lista[0] += num;
     document.getElementById('display-calculator').value = lista[0];
+    if (lista[0] != '') {
+      console.log(lista[0]);
+      document.getElementById('buttonClean').innerText = 'C';
+    }
   } else if (lista.length >= 1 && simbol != '') {
     lista[1] += num;
     document.getElementById('display-calculator').value = lista[1];
+    console.log(lista[1]);
   }
 
-  console.log(lista);
-}
 
-
-
-function verificaSeCaractereOperadorMatematico(caracter) {
-  return caracter == "+" || caracter == "-" || caracter == "*" || caracter == "/" || caracter == "="
 
 }
-
 
 
 // guardar  o operador
 
 function operator(value) {
-  if (lista[0] && lista[1]) {
-    let resultado = calcular(parseFloat(lista[0]), parseFloat(lista[1]), simbol);
-    document.getElementById('display-calculator').value = `${resultado}`
+  if (lista[0] && lista[1] != '') {
+    let resultado = calcular(parseFloat(lista[0].replace(",", ".")), parseFloat(lista[1].replace(",", ".")), simbol);
+    // document.getElementById('display-calculator').value = `${resultado}`
     lista[0] = `${resultado}`;
-    lista[1] = ''
-    return
+    lista[1] = '';
+    // return
   }
-
-  document.getElementById('display-calculator').value = '';
+  // document.getElementById('display-calculator').value = '';
   simbol = value;
   console.log(simbol)
 
 }
+
+
 
 
 function calcular(num1 = parseFloat(lista[0]), num2 = parseFloat(lista[1]), operacao = simbol) {
@@ -56,22 +58,35 @@ function calcular(num1 = parseFloat(lista[0]), num2 = parseFloat(lista[1]), oper
     return null;
   }
 
+  let result = 0
+
   switch (operacao) {
     case '+':
-      document.getElementById('display-calculator').value = num1 + num2;
-      return num1 + num2;
+      result = num1 + num2
+      setDisplay(result)
+      return result;
 
     case '-':
-      document.getElementById('display-calculator').value = num1 - num2;
-      return num1 - num2;
+      result = num1 - num2
+      setDisplay(result)
+      return result;
 
     case '*':
-      document.getElementById('display-calculator').value = num1 * num2;
-      return num1 * num2;
+      result = num1 * num2
+      setDisplay(result)
+      return result;
 
     case '/':
-      document.getElementById('display-calculator').value = num1 / num2;
-      return num1 / num2;
+      result = num1 / num2
+      setDisplay(result)
+      return result;
+
+    case '%':
+
+      result = (num1 / num2) * 100;
+      setDisplay(result)
+      return result;
+
 
     default:
       break;
@@ -79,130 +94,55 @@ function calcular(num1 = parseFloat(lista[0]), num2 = parseFloat(lista[1]), oper
 }
 
 
-// function igual() {
-//   if (sinal == "+") {
-//       resultadoFinal = parseFloat(primeiroValor) + parseFloat(segundoValor)
-//   }
-//   if (sinal == "/") {
-//       resultadoFinal = parseFloat(primeiroValor) / parseFloat(segundoValor)
-//   }
-//   if (sinal == "-") {
-//       resultadoFinal = parseFloat(primeiroValor) - parseFloat(segundoValor)
-//   }
-//   if (sinal == "x") {
-//       resultadoFinal = parseFloat(primeiroValor) * parseFloat(segundoValor)
-//   }
-//   resultado.innerHTML = resultadoFinal
-//   primeiroValor = resultadoFinal
-//   temVirgula = false
-//   segundoValor = ''
-//   checarTamanhoResultado()
-// }
+function setDisplay(num) {
+  document.getElementById('display-calculator').value = `${num}`.replace(".", ",")
+  // Formata o número com pontos nas casas decimais
+  let formattedNum = num.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 10 });
+  document.getElementById('display-calculator').value = formattedNum.replace(",", ".");
 
-// function checarTamanhoResultado() {
-//   resultadoFinal = String(resultadoFinal)
-//   if (resultadoFinal.length >= 8) {
-//       resultadoFinal = JSON.parse(resultadoFinal);
-//       resultado.innerHTML = resultadoFinal.toFixed(4)
-//   }
-// }
+}
 
 
 
 
+function percente() {
+  if (lista[0] != '') {
+    let resultado = document.getElementById('display-calculator').value
+    resultado = parseFloat(lista[0]) / 100
+    lista[0] = resultado
+    console.log(resultado)
+    document.getElementById('display-calculator').value = resultado
+
+  }
+}
+
+
+function negative() {
+
+  let resultado = document.getElementById('display-calculator').value
+  resultado = (lista[0]) * -1
+  lista[0] = resultado
+  console.log(resultado)
+  lista[0] = resultado
+  document.getElementById('display-calculator').value = resultado
 
 
 
-
-
-
-
-
-
-
-
+}
 
 // função para limpar a tela
 
 function clean() {
+
   document.getElementById('display-calculator').value = ""
   lista = ['', ''];
   simbol = '';
-  console.log(lista);
+  document.getElementById('buttonClean').innerText = 'AC';
+
+
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//variaveis necessárias para  a operação
-
-// let firstOperation
-// let secondOperation
-// let operation 
-
-
-// function calculate() {
-//   if (operator === null || firstValue === null) return;
-//   let secondOperand = parseFloat(temporaryNumber.replace(",", "."));
-//   let resultValue;
-
-//   switch (operator) {
-//     case "+":
-//       resultValue = firstValue + secondOperand;
-//       break;
-//     case "-":
-//       resultValue = firstValue - secondOperand;
-//       break;
-//     case "×":
-//       resultValue = firstValue * secondOperand;
-//       break;
-//     case "÷":
-//       resultValue = firstValue / secondOperand;
-//       break;
-//     default:
-//       return;
-//   }
-
-
-//   operator = null;
-//   firstValue = null;
-//   restart = true;
-//   percentageValue = null;
-//   updateResult();
-
-
-
-// }
-
-// function setOperator(newOperator) {
-//   if (temporaryNumber) {
-//     calculate();
-
-//     firstValue = parseFloat(temporaryNumber.replace(",", "."));
-//     temporaryNumber = "";
-//   }
-
-//   operator = newOperator;
-// }
